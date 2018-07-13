7091f094b7ebbc255e3ef554e619e89a777c1d33
Ross Kaffenberger <rosskaff@gmail.com>
2018-07-13 16:24:31 -0400

    Rename test application

--- spec/sample/config/environments/development.rb
+++ spec/sample/config/environments/development.rb
@@ -1,4 +1,4 @@
-Dummy::Application.configure do
+Sample::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
@@ -24,9 +24,5 @@ Dummy::Application.configure do
 
   # Raise exception on mass assignment protection for Active Record models
   # config.active_record.mass_assignment_sanitizer = :strict
-
-  # Log the query plan for queries taking more than this (works
-  # with SQLite, MySQL, and PostgreSQL)
-  config.active_record.auto_explain_threshold_in_seconds = 0.5
 
 end
